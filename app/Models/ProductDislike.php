6bdd2e26dc687e62001555f361527e5ff78a2623
unknown <shankhantanoli1@gmail.com>
2022-06-20 15:46:38 +0500

    New Update

--- app/Models/ProductDislike.php
+++ app/Models/ProductDislike.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ProductdisLike extends Model
+class ProductDisLike extends Model
 {
     use HasFactory;
 }
